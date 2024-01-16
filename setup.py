bbe8e6a4f03dfa3ccc42f0f10db51038effaf489
Benedikt Mersch <benedikt.mersch@gmail.com>
2024-01-16 15:56:23 +0100

    Relax these

--- setup.py
+++ setup.py
@@ -8,12 +8,11 @@ setup(
     description="Receding Moving Object Segmentation in 3D LiDAR Data Using Sparse 4D Convolutions",
     packages=find_packages(where="src"),
     install_requires=[
-        "Click>=7.0",
-        "numpy>=1.20.3",
-        "pytorch_lightning>=1.6.4",
-        "PyYAML>=6.0",
-        "tqdm>=4.62.3",
-        "torch",
-        "ninja",
+        "Click",
+        "numpy",
+        "pytorch_lightning",
+        "tensorboard",
+        "PyYAML",
+        "tqdm",
     ],
 )
